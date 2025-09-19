932f57e50582c54ceb7626a931e5906189f1b05a
siwakasen <siwakasen@gmail.com>
2025-09-19 16:31:35 +0700

    fix: authorization for report service

--- apps/report-service/src/api/report.controller.ts
+++ apps/report-service/src/api/report.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import {  GetExpensesYearlyReportDto, GetExpensesYearComparisonReportDto, GetBookingYearlyRevenueDto, GetBookingYearComparisonRevenueDto, GetProfitAndLossStatementYearlyReportDto, GetProfitAndLossStatementYearComparisonReportDto, GetBookingMonthlyRevenueDto, GetExpensesMonthlyReportDto } from './report.dto';
-
+import {
+  GetExpensesYearlyReportDto,
+  GetExpensesYearComparisonReportDto,
+  GetBookingYearlyRevenueDto,
+  GetBookingYearComparisonRevenueDto,
+  GetProfitAndLossStatementYearlyReportDto,
+  GetProfitAndLossStatementYearComparisonReportDto,
+  GetBookingMonthlyRevenueDto,
+  GetExpensesMonthlyReportDto,
+} from './report.dto';
+import { JwtAuthGuard } from '@app/helpers/auth/user/auth.guard';
+import { Roles, UserType } from '@app/helpers/auth/decorators/auth.decorator';
 
 @ApiBearerAuth()
 @ApiTags('Report')
@@ -10,44 +20,69 @@ import {  GetExpensesYearlyReportDto, GetExpensesYearComparisonReportDto, GetBoo
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('monthly/bookings')
   getBookingMonthlyRevenue(@Query() payload: GetBookingMonthlyRevenueDto) {
     return this.reportService.getBookingMonthlyRevenue(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('yearly/bookings')
-    getBookingYearlyRevenue(@Query() payload: GetBookingYearlyRevenueDto){
-      return this.reportService.getBookingYearlyRevenue(payload);
+  getBookingYearlyRevenue(@Query() payload: GetBookingYearlyRevenueDto) {
+    return this.reportService.getBookingYearlyRevenue(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('year-comparison/bookings')
-  getBookingYearComparisonRevenue(@Query() payload: GetBookingYearComparisonRevenueDto) {
+  getBookingYearComparisonRevenue(
+    @Query() payload: GetBookingYearComparisonRevenueDto
+  ) {
     return this.reportService.getBookingYearComparisonRevenue(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('monthly/expenses')
   getExpensesMonthlyReport(@Query() payload: GetExpensesMonthlyReportDto) {
     return this.reportService.getExpensesMonthlyReport(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('yearly/expenses')
   getExpensesYearlyReport(@Query() payload: GetExpensesYearlyReportDto) {
     return this.reportService.getExpensesYearlyReport(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('year-comparison/expenses')
-  getExpensesYearComparisonReport(@Query() payload: GetExpensesYearComparisonReportDto) {
+  getExpensesYearComparisonReport(
+    @Query() payload: GetExpensesYearComparisonReportDto
+  ) {
     return this.reportService.getExpensesYearComparisonReport(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('yearly/profit-loss')
-  getProfitAndLossStatementYearlyReport(@Query() payload: GetProfitAndLossStatementYearlyReportDto) {
+  getProfitAndLossStatementYearlyReport(
+    @Query() payload: GetProfitAndLossStatementYearlyReportDto
+  ) {
     return this.reportService.getProfitAndLossStatementYearlyReport(payload);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserType.OWNER)
   @Get('year-comparison/profit-loss')
-  getProfitAndLossStatementYearComparisonReport(@Query() payload: GetProfitAndLossStatementYearComparisonReportDto) {
-    return this.reportService.getProfitAndLossStatementYearComparisonReport(payload);
+  getProfitAndLossStatementYearComparisonReport(
+    @Query() payload: GetProfitAndLossStatementYearComparisonReportDto
+  ) {
+    return this.reportService.getProfitAndLossStatementYearComparisonReport(
+      payload
+    );
   }
-
 }
--- apps/report-service/src/api/report.module.ts
+++ apps/report-service/src/api/report.module.ts
@@ -1,10 +1,52 @@
 import { Module } from '@nestjs/common';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ClientGrpc, ClientsModule, Transport } from '@nestjs/microservices';
+import { AuthHelper } from '@app/helpers/auth/user/auth.helper';
+import { JwtStrategy } from '@app/helpers/auth/user/auth.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'user', property: 'user' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_KEY'),
+        signOptions: { expiresIn: config.get('JWT_EXPIRES') },
+      }),
+    }),
+    ClientsModule.registerAsync([
+      {
+        inject: [ConfigService],
+        name: 'EMP_AUTH_CLIENT',
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            package: 'authemp',
+            protoPath: 'contract/auth-employee-api.proto',
+            url: config.get<string>('EMP_AUTH_CLIENT'),
+            loader: {
+              keepCase: true,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [ReportController],
-  providers: [ReportService],
+  providers: [
+    ReportService,
+    JwtStrategy,
+    {
+      provide: AuthHelper,
+      useFactory: (jwt: JwtService, clientEmp: ClientGrpc) => {
+        return new AuthHelper(jwt, clientEmp);
+      },
+      inject: [JwtService, 'EMP_AUTH_CLIENT'],
+    },
+  ],
 })
 export class ReportModule {}
