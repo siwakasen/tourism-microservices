415c33077f7186b9e97b216052e40f37f887cc23
siwakasen <siwakasen@gmail.com>
2025-11-03 21:35:16 +0700

    fix: resolve env runtime variables on all services

--- apps/travel-packages-service/src/shared/typeorm/typeorm.service.ts
+++ apps/travel-packages-service/src/shared/typeorm/typeorm.service.ts
@@ -9,11 +9,6 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   private readonly config: ConfigService;
 
   public createTypeOrmOptions(): TypeOrmModuleOptions {
-    console.log('DATABASE_HOST:', this.config.get<string>('DATABASE_HOST'));
-    console.log(
-      'SLAVE_DATABASE_HOST:',
-      this.config.get<string>('SLAVE_DATABASE_HOST')
-    );
     return {
       type: 'postgres',
       replication: {
